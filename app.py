343e623f0fa5162c4adc37c54d27373f5a90837c
moustafa-7 <moustafasleem777@gmail.com>
2021-07-13 05:40:20 +0200

    docker up and running

--- app.py
+++ app.py
@@ -3,56 +3,61 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-from sklearn.externals import joblib
+
+# from sklearn.externals import joblib
+import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
+
 def scale(payload):
     """Scales Payload"""
-    
+
     LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
+
 
 @app.route("/")
 def home():
     html = f"<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
-@app.route("/predict", methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
     """Performs an sklearn prediction
-        
-        input looks like:
-        {
-        "CHAS":{
-        "0":0
-        },
-        "RM":{
-        "0":6.575
-        },
-        "TAX":{
-        "0":296.0
-        },
-        "PTRATIO":{
-        "0":15.3
-        },
-        "B":{
-        "0":396.9
-        },
-        "LSTAT":{
-        "0":4.98
-        }
-        
-        result looks like:
-        { "prediction": [ <val> ] }
-        
-        """
-    
+
+    input looks like:
+    {
+    "CHAS":{
+    "0":0
+    },
+    "RM":{
+    "0":6.575
+    },
+    "TAX":{
+    "0":296.0
+    },
+    "PTRATIO":{
+    "0":15.3
+    },
+    "B":{
+    "0":396.9
+    },
+    "LSTAT":{
+    "0":4.98
+    }
+
+    result looks like:
+    { "prediction": [ <val> ] }
+
+    """
+
     # Logging the input payload
     json_payload = request.json
     LOG.info(f"JSON payload: \n{json_payload}")
@@ -63,9 +68,10 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    return jsonify({'prediction': prediction})
+    return jsonify({"prediction": prediction})
+
 
 if __name__ == "__main__":
     # load pretrained model as clf
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    app.run(host="0.0.0.0", port=80, debug=True)  # specify port=80
